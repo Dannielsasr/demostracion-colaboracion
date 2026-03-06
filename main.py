5fcc1f6d9da90db5caeecd1ab02749d8476a4799
SanPiter <pitersan106@gmail.com>
2026-03-06 07:36:10 -0500

    Fibonacci

--- main.py
+++ main.py
@@ -7,3 +7,11 @@ def suma(a, b):
 
 def resta(a, b):
     return a - b
+
+def fibonacci(n):
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
